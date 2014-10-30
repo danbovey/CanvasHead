2650345330788b7d02aecc04de389d0b900a8788
Dan Bovey <dan@danbovey.co.uk>
2014-10-30 20:22:23 +0000

    Bullet physics

--- js/gameLoop.js
+++ js/gameLoop.js
@@ -17,31 +17,6 @@ function gameLoop() {
 	}
 }
 
-function update() {
-	waves();
-	ai();
-	collision();
-	ui();
-
-	if(health <= 0) {
-		scene.objects.push(gameOver);
-		game = false;
-	}
-
-	// Restart
-	if(keys[82]) {
-		window.cancelAnimFrame(request);
-		request = false;
-
-		init();
-	}
-}
-
-function render() {
-	scene.draw(canvas, ctx);
-	request = requestAnimFrame(gameLoop);
-}
-
 function input() {
 	// W
 	if(keys[87]) {
@@ -82,84 +57,101 @@ function input() {
 	if(keys[32]) {
 		shoot();
 	}
+
+	// Restart
+	if(keys[82]) {
+		window.cancelAnimFrame(request);
+		request = false;
+
+		init();
+	}
+}
+
+function update() {
+	waves();
+	ai();
+	collision();
+
+	upgrades();
+	ui();
+
+	if(health <= 0) {
+		scene.objects.push(gameOver);
+		game = false;
+	}
+}
+
+function render() {
+	scene.draw(canvas, ctx);
+	request = requestAnimFrame(gameLoop);
 }
 
 function shoot() {
 	if(shooting === false) {
 		shooting = true;
+
+		var bullet = new Rectangle({
+			id: bulletID,
+			type: 'bullet',
+			ctx: ctx,
+			x: 0,
+			y: 0,
+			x2: 0,
+			y2: 0,
+			width: 10,
+			height: 10,
+			fillStyle: '#AAAAAA'
+		});
+
 		switch(player.direction) {
 			case 'n':
-				var bullet = new Rectangle({
-					id: bulletID,
-					type: 'bullet',
-					ctx: ctx,
-					x: player.x + (player.width / 2),
-					y: player.y - 8,
-					x2: player.x + (player.width / 2),
-					y2: -8,
-					width: 2,
-					height: 8,
-					fillStyle: '#AAAAAA'
-				});
+				bullet.x = player.x + (player.width / 2);
+				bullet.x2 = player.x + (player.width / 2);
+				bullet.y = player.y - 8;
+				bullet.y2 = -8;
+				bullet.width = 2;
+				bullet.height = 8;
 			break;
 			case 'e':
-				var bullet = new Rectangle({
-					id: bulletID,
-					type: 'bullet',
-					ctx: ctx,
-					x: player.x + player.width,
-					y: player.y + (player.height / 2),
-					x2: canvas.width + 8,
-					y2: player.y + (player.height / 2),
-					width: 8,
-					height: 2,
-					fillStyle: '#AAAAAA'
-				});
+				bullet.x = player.x + player.width;
+				bullet.x2 = canvas.width + 8;
+				bullet.y = player.y + (player.height / 2);
+				bullet.y2 = player.y + (player.height / 2);
+				bullet.width = 8;
+				bullet.height = 2;
 			break;
 			case 's':
-				var bullet = new Rectangle({
-					id: bulletID,
-					type: 'bullet',
-					ctx: ctx,
-					x: player.x + (player.width / 2),
-					y: player.y + player.height + 8,
-					x2: player.x + (player.width / 2),
-					y2: canvas.height + 8,
-					width: 2,
-					height: 8,
-					fillStyle: '#AAAAAA'
-				});
+				bullet.x = player.x + (player.width / 2);
+				bullet.x2 = player.x + (player.width / 2);
+				bullet.y = player.y + player.height;
+				bullet.y2 = canvas.height + 8;
+				bullet.width = 2;
+				bullet.height = 8;
 			break;
-			case 'w':
-				var bullet = new Rectangle({
-					id: bulletID,
-					type: 'bullet',
-					ctx: ctx,
-					x: player.x - 8,
-					y: player.y + (player.height / 2),
-					x2: -8,
-					y2: player.y + (player.height / 2),
-					width: 8,
-					height: 2,
-					fillStyle: '#AAAAAA'
-				});
+			default:
+				bullet.x = player.x - 8;
+				bullet.x2 = -8;
+				bullet.y = player.y + (player.height / 2);
+				bullet.y2 = player.y + (player.height / 2);
+				bullet.width = 8;
+				bullet.height = 2;
 			break;
 		}
-
 		
-		scene.objects.push(bullet);
+		addObject(bullet);
 
 		bulletID++;
 
 		window.setTimeout(function() {
 			shooting = false;
-		}, 500);
+		}, 400);
 	}
 }
 
 function collision() {
 	var indexesToRemove = [];
 	for(var i = 0; i < scene.objects.length; i++) {
+		// Check that the player is not outside of the canvas
 		if(scene.objects[i].type === 'player') {
 			if(scene.objects[i].x < 0 || scene.objects[i].x + scene.objects[i].width > canvas.width) {
 				scene.objects[i].x = scene.objects[i].lastX;
@@ -168,6 +160,15 @@ function collision() {
 				scene.objects[i].y = scene.objects[i].lastY;
 			}
 		}
+
+		// Check that bullets are not outside of the canvas
+		if(scene.objects[i].type === 'bullet') {
+			if(scene.objects[i].x < 0 || scene.objects[i].x + scene.objects[i].width > canvas.width || scene.objects[i].y < 0 || scene.objects[i].y + scene.objects[i].height > canvas.height) {
+				indexesToRemove.push(scene.objects[i]);
+			}
+		}
+
+		// Collision checks
 		for(var x = 0; x < scene.objects.length; x++) {
 			if(scene.objects[i].type !== 'ui' && scene.objects[x].type !== 'ui') {
 				if(Math.ceil(scene.objects[i].x) + scene.objects[i].width === Math.ceil(scene.objects[x].x) && (Math.ceil(scene.objects[i].y) >= Math.ceil(scene.objects[x].y) && Math.ceil(scene.objects[i].y) <= Math.ceil(scene.objects[x].y) + scene.objects[x].height || Math.ceil(scene.objects[i].y) + scene.objects[i].height >= Math.ceil(scene.objects[x].y) && Math.ceil(scene.objects[i].y) + scene.objects[i].height <= Math.ceil(scene.objects[x].y) + scene.objects[x].height)) { // East
@@ -205,54 +206,55 @@ function collision() {
 				}
 			}
 
-			if(scene.objects[i].type === 'player' && scene.objects[x].type === 'zombie') {
-				if(scene.objects[i].x < scene.objects[x].x + scene.objects[x].width  && scene.objects[i].x + scene.objects[i].width  > scene.objects[x].x && scene.objects[i].y < scene.objects[x].y + scene.objects[x].height && scene.objects[i].y + scene.objects[i].height > scene.objects[x].y) {
+			// If an object is inside another object
+			if(scene.objects[i].x < scene.objects[x].x + scene.objects[x].width  && scene.objects[i].x + scene.objects[i].width  > scene.objects[x].x && scene.objects[i].y < scene.objects[x].y + scene.objects[x].height && scene.objects[i].y + scene.objects[i].height > scene.objects[x].y) {
+				if(scene.objects[i].type === 'player' && scene.objects[x].type === 'zombie') {
 					player.x = player.lastX;
 					player.y = player.lastY;
 				}
-			}
 
-			if(scene.objects[i].type === 'bullet') {
-				// if(scene.objects[i] !== scene.objects[x]) {
-					if(scene.objects[i].x < 0 || scene.objects[i].x + scene.objects[i].width > canvas.width || scene.objects[i].y < 0 || scene.objects[i].y + scene.objects[i].height > canvas.height) {
-						// scene.objects[i].fillStyle = 'transparent';
-						console.log('canvas ' + i);
-						indexesToRemove.push(i);
+				if(scene.objects[i].type === 'bullet' && scene.objects[x].type === 'wall') {
+					indexesToRemove.push(scene.objects[i]);
+				}
+				if(scene.objects[i].type === 'bullet' && scene.objects[x].type === 'zombie') {
+					indexesToRemove.push(scene.objects[i]);
+					scene.objects[x].health = scene.objects[x].health - damage;
+					if(scene.objects[x].health <= 0) {
+						indexesToRemove.push(scene.objects[x]);
+
+						score = score + combo;
 					}
-					if(scene.objects[i].x < scene.objects[x].x + scene.objects[x].width  && scene.objects[i].x + scene.objects[i].width  > scene.objects[x].x && scene.objects[i].y < scene.objects[x].y + scene.objects[x].height && scene.objects[i].y + scene.objects[i].height > scene.objects[x].y) {
-						if(scene.objects[x].type === 'zombie') {
-							// scene.objects[x].kill = true;
-							console.log('zombie');
-							indexesToRemove.push(i);
-							indexesToRemove.push(x);
-						} else {
-							console.log('other');
-							indexesToRemove.push(i);
-						}
-					}
-				// }
+				}
 			}
 		}
 	}
 
 	for(var i = 0; i < indexesToRemove.length; i++) {
-		
+		removeObject(indexesToRemove[i]);
 	}
+	indexesToRemove = [];
 }
 
-var wavesStarted = false;
+// var wavesStarted = false;
+var waveTime = 12000;
+var waveCountdown = window.setInterval(function() {
+	wave++;
+	waveTime = waveTime * 2;
+	console.log(wave);
+}, waveTime);
 
 function waves() {
-	if(wavesStarted === false) {
-		wavesStarted = true;
+	// if(wavesStarted === false) {
+		// wavesStarted = true;
 		if(wavesEntered[wave] == undefined) {
-			for(var i = 0, algorithm = Math.ceil(wave * wave + 3); i <= algorithm; i++) {
+			for(var i = 0, algorithm = Math.ceil(wave * 2); i <= algorithm; i++) {
 				if(i < algorithm / 2) {
 					var zombie = new Rectangle({
 						id: zombieID,
 						type: 'zombie',
+						health: 2,
 						ctx: ctx,
-						x: Math.floor(Math.random()*(800-0+1)+0),
+						x: Math.floor(Math.random() * (800 - 0 + 1) + 0),
 						y: -36,
 						width: 24,
 						height: 36,
@@ -264,8 +266,9 @@ function waves() {
 					var zombie = new Rectangle({
 						id: zombieID,
 						type: 'zombie',
+						health: 2,
 						ctx: ctx,
-						x: Math.floor(Math.random()*(800-0+1)+0),
+						x: Math.floor(Math.random() * (800 - 0 + 1) + 0),
 						y: canvas.height,
 						width: 24,
 						height: 36,
@@ -275,13 +278,13 @@ function waves() {
 					});
 				}
 				zombies.push(zombie);
-				scene.objects.push(zombie);
+				addObject(zombie);
 
 				zombieID++;
 			}
+			wavesEntered.push(wave);
 		}
-		wavesEntered.push(wave);
-	}
+	// }
 }
 
 function ai() {
@@ -310,24 +313,43 @@ function ai() {
 		}
 		 else if(object.type === 'bullet') {
 			// Bullets
-			if(object.x < object.x2) {
-				object.x = object.x + 6;
-			} else {
-				object.x = object.x - 6;
+			if(object.x !== object.x2) {
+				if(object.x < object.x2) {
+					object.x = object.x + 8;
+				} else {
+					object.x = object.x - 8;
+				}
 			}
 
-			if(object.y < object.y2) {
-				object.y = object.y + 6;
-			} else {
-				object.y = object.y - 6;
-			}			
+			if(object.y !== object.y2) {
+				if(object.y < object.y2) {
+					object.y = object.y + 8;
+				} else {
+					object.y = object.y - 8;
+				}
+			}
+		}
+	}
+}
+
+function upgrades() {
+	if(upgrade !== 10) { // Max amount of upgrades
+		if(score >= 200) {
+			upgrade = 2;
+		} else if(score >= 100) {
+			upgrade = 1;
 		}
 	}
 }
 
 function ui() {
 	if(health >= 0) {
-		healthFG.width = (health / 100) * 150;
+		healthFG.width = (health / 100) * 24;
 	}
-	scoreText.text = score;
+	healthFG.x = player.x;
+	healthBG.x = player.x;
+	healthFG.y = player.y - 10;
+	healthBG.y = player.y - 10;
+
+	scoreText.text = pad(score, 8);
 }
